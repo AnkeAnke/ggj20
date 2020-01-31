21bc843a8693b865c4d67fc20244771f800323dd
Anke <ankef@kth.se>
2020-01-31 23:46:13 +0100

    Keyboard setup

---
+++ Keyboard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MonoGame;
+
+namespace ggj20
+{
+    /// <summary>
+    /// Everything about keyboard use.
+    /// * Render keyboard
+    /// * Know key positions
+    /// * Can tell letter distance for a point
+    /// </summary>
+
+    class Keyboard
+    {
+        Texture texture;
+        Vector2 letterPositions[26];
+        Vector2 offset;
+        float scale;
+
+        void Draw() { }
+        void Update(){}
+
+        // GetLetterDistances
+        // 
+
+    }
+}
